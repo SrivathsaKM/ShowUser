d82b5d0c4082af1f263374afd8b11d7d3294c005
Srivathsa KM <srivathsakm@gmail.com>
2021-07-26 13:33:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import DataGrid from 'react-data-grid';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState({});
+  const [name, setName] = useState('');
+  const [flag, setFlag] = useState(false);
+
+  const handleClick = () => {
+    setCount(count + 2);
+  };
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name.length === 0) {
+      alert('Please Enter Name');
+    } else {
+      axios
+        .get(`https://api.nationalize.io/?name=${name}`)
+        .then((response) => {
+          setUsers(response.data);
+          //console.log(response.data);
+          setFlag(true);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+  const columns = [
+    { key: 'id', name: '#' },
+    { key: 'country_id', name: 'Country ID' },
+    { key: 'probability', name: 'Probability' },
+  ];
+
+  const rows = [];
+  console.log(rows);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {count}
+      <button onClick={handleClick}>click</button>
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={handleChange} value={name} />
+        <button type='submit'>submit</button>
+      </form>
+      {flag ? (
+        users.country.length > 0 ? (
+          <>
+            {users.country.map((ele, idx) => {
+              rows.push({ id: idx + 1, country_id: ele.country_id, probability: ele.probability });
+            })}
+            <DataGrid columns={columns} rows={rows} />
+          </>
+        ) : (
+          <p style={{ color: 'red' }}>No matching result for name '{users.name}'</p>
+        )
+      ) : (
+        ''
+      )}
     </div>
   );
-}
+};
 
 export default App;
